cf890a50e5f2f33935c814f7273cdc93095d11cc
李世杰 <lisj@mxchip.com>
2019-08-07 14:43:22 +0800

    :sparkles: Single-spa Example

---
+++ README.md
@@ -0,0 +1,23 @@
+<h2 style="text-align: center">Single-spa Example</h2>
+
+---
+
+### 概览
+
+`single-spa` 是一个在前端应用程序中汇集多个 `javascript` 微内容的框架。使用单一水疗中心构建您的前端可带来许多好处，例如：
+
+- 在同一页面上使用多个框架而无需刷新页面 （`React`，`AngularJS`，`Angular`，`Ember`或您正在使用的任何内容）
+- 独立部署微内容。
+- 使用新框架编写代码，而无需重写现有应用程序
+- 延迟加载代码，用于改善初始加载时间。
+
+### 运行Demo
+
+``` sh
+# clone project
+git clone https://github.com/LishiJ/single-spa_example.git
+# yarn / npm install
+yarn
+# run
+yarn start
+```
---
+++ single-spa.config.js
@@ -0,0 +1,35 @@
+import { registerApplication, start } from 'single-spa';
+
+function pathPrefix(prefix) {
+  return function(location) {
+      return location.pathname.startsWith(prefix);
+  }
+}
+
+// 导航栏
+registerApplication(
+  'navBar', 
+  () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
+  () => true
+);
+
+// 首页即React页面
+registerApplication(
+  // Name of our single-spa application
+  'home',
+  // loadingFunction
+  () => import('./src/home/home.app.js'),
+  // activityFunction
+  (location) => location.pathname === "" || 
+    location.pathname === "/" || 
+    location.pathname.startsWith('/home')
+);
+
+// angular页面
+registerApplication(
+  'angularJS', 
+  () => import ('./src/angularJS/angularJS.app.js'), 
+  pathPrefix('/angularJS')
+);
+
+start();
